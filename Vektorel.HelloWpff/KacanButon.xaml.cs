a6398e7109bff0dca51eecc870526ca152a5a4f2
kemaldemirbas066@gmail.com <kemaldemirbas066@gmail.com>
2020-07-19 13:22:10 +0300

    Kaçan butonda değişişlik yapıldı

--- Vektorel.HelloWpff/KacanButon.xaml.cs
+++ Vektorel.HelloWpff/KacanButon.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Vektorel.HelloWpff
 {
@@ -19,44 +21,66 @@ namespace Vektorel.HelloWpff
     /// </summary>
     public partial class KacanButon : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
         int puan;
-        string txtad;
-        string txtsoyad;
+        int sayi = 10;
         public KacanButon()
         {
             InitializeComponent();
         }
-        public KacanButon(string txtad, string txtsoyad)
+        public KacanButon(string ad, string soyad)
         {
             InitializeComponent();
-            lbl2.Content =$"Hoşgeldiniz  {txtad}  {txtsoyad}";
+            lbl2.Content = $"Hoşgeldiniz  {ad}  {soyad}";
+
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
 
         }
-            private void Button_MouseMove(object sender, MouseEventArgs e)
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (sayi == 0)
             {
-                Random rnd = new Random();
-                int sol = rnd.Next((int)grd.ColumnDefinitions[0].ActualWidth - (int)btnkacan.Width);
-                int top = rnd.Next((int)grd.ActualHeight - (int)btnkacan.Height);
-                btnkacan.Margin = new Thickness(sol, top, 0, 0);
-
-                Button btn = (Button)sender;
-
-                //if (btn.Margin.Left - btnkacan.Margin.Left > 50)
-                //{
-                //    puan += 10;
-                //}
-
-                //else
-                //{
-                //    puan++;
-                //}
-
-
-                puan++;
-
-                //btnkacan.Content = puan;
-                lbl.Content = puan;
+                timer.Stop();
+                btnkacan.Visibility = Visibility.Hidden;
+                MessageBox.Show($"oyun bitti\npuanınız:{puan}");
+            }
+            else
+            {
+                sayi--;
+                lbl3.Content = sayi;
                 
             }
+
+        }
+
+        private void Button_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random rnd = new Random();
+            int sol = rnd.Next((int)grd.ColumnDefinitions[0].ActualWidth - (int)btnkacan.Width);
+            int top = rnd.Next((int)grd.ActualHeight - (int)btnkacan.Height);
+            btnkacan.Margin = new Thickness(sol, top, 0, 0);
+
+            Button btn = (Button)sender;
+
+            //if (btn.Margin.Left - btnkacan.Margin.Left > 50)
+            //{
+            //    puan += 10;
+            //}
+
+            //else
+            //{
+            //    puan++;
+            //}
+
+            puan++;
+
+            //btnkacan.Content = puan;
+            lbl.Content = puan;
+
         }
     }
+}
